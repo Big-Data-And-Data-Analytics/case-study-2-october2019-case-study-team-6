02b6bcf7354062f351d69057b1535699ee2a4ab3
Max Kinner <maximiliankinner@gmail.com>
2020-10-17 18:52:08 +0200

    Class structure update; Claning R->Python migration update

--- scripts/03 Data Preparation/01 Cleaning/Cleaning.py
+++ scripts/03 Data Preparation/01 Cleaning/Cleaning.py
@@ -1,43 +1,72 @@
 class Cleaning:
+    """
+    This class provides functions for cleaning the data.
+    """
+
+    version = "0.1"
+
     def __init__(self):
         pass
 
     def removeWhiteSpaces(self, data, column): # Remove whitepsaces in the given data-column
+        """
+        Removes all whitespaces before and after the text and multi whitespaces inside the text
+
+        :type data: pandas dataframe
+        :param data: which dataframe should be used
+        
+        :type column: string
+        :param column: the name of the column
+        """
+
         data[column] = data[column].str.strip()
         data[column] = data[column].str.replace("  +", " ")
+        print("Whitespaces removed!")
         return data
-
-    #def removeLineBreaks(self, data, column): # Remove linebreaksa in the given data-column; MongoDB won't allow linebreaks in the JSON, so this step is not necessary at this point and should be catched right after crawling, before importing to MongoDB
-    #    data[column] = data[column].replace(to_replace="[\n\r]", value=" ", regex=True)
-    #    print(f'Linebreaks removed from column {column}')
-    #    return data
 
     def changeEmptyToNA(self, data, column): # Change empty strings to NA in the given data-column
-        data[column].replace(r'^\s*$', np.nan, regex=True)
+        """
+        Changes all empty strings ("") to NA
 
-    def removeDuplicates(self, data): # Remove duplicates
-        data = data.drop(columns="_id")
-        data = data.drop_duplicates()
-        print(f'Duplicates removed from column {data}')
+        :type data: pandas dataframe
+        :param data: which dataframe should be used
+        
+        :type column: string
+        :param column: the name of the column
+        """
+
+        data[column] = data[column].replace(r'^\s*$', np.nan, regex=True)
+        print("Empty strings chaned!")
         return data
 
-########
-from pymongo import MongoClient
-import pandas as pd
-import numpy as np
+    def removeDuplicates(self, data): # Remove duplicates
+        """
+        Removes duplicated rows
 
-client = MongoClient('localhost', 27017)
-db = client['01_NationalIdentity_Crawled']
-data = db.youTube_Videos_Raw
-data = data.find({})
-data = list(data)
+        :type data: pandas dataframe
+        :param data: which dataframe should be used
+        """
+
+        data = data.drop(columns="_id")
+        data = data.drop_duplicates()
+        print("Duplicates removed!")
+        return data
 
 
-data = pd.DataFrame(list())
+if __name__ == "__main__":
+    from pymongo import MongoClient
+    import pandas as pd
+    import numpy as np
 
-cleaner = Cleaning()
+    client = MongoClient('localhost', 27017)
+    db = client['01_NationalIdentity_Crawled']
+    data = db.youTube_Video_Comments_Raw
+    data = data.find({})
+    data = list(data)
+    data = pd.DataFrame(data)
 
-# Encodinc issue
-# reddit
-# youtube comments
-# youtube videos
+    cleaner = Cleaning()
+
+    data = cleaner.removeDuplicates(data)
+    data = cleaner.removeWhiteSpaces(data, "textOriginal")
+    data = cleaner.changeEmptyToNA(data, "textOriginal")
--- scripts/Class Structure.py
+++ scripts/Class Structure.py
@@ -5,9 +5,6 @@ class Cleaning:
     def removeWhiteSpaces(self, data, column): # Remove whitespaces in the given data.column
         pass
 
-    def removeLineBreaks(self, data, column): # Remove linebreaksa in the given data.column
-        pass
-
     def changeEmptyToNA(self, data, column): # Change empty strings to NA in the given data.column
         pass
 
@@ -40,27 +37,85 @@ class Sentiment:
 class National_Identity_Extraction:
     def __init__(self):
 		pass
+
+    def find_national_identity(self, data, column):
+        pass
+
+    def extract_country_emojis(self, flags_set, emojis):
+        pass
+
+    def remove_emojis_from_onlytext(self, data, column):
+        pass
+
+    def extract_emojis(self, onlyText):
+        pass
+
+    def lang_detect(self, data, column):
+        pass
+
+    def lang_id(self, data, column):
+        pass
+
+    def call_Functions(self, data, column):
+        pass
 ######################################################################
 class Translation:
     def __init__(self):
 		pass
+
+    def translate(self, dataframe):
+        pass
 ######################################################################
-class Text_Cleaning: # Add to class Cleaning
+class Text_Cleaning: # A separate text cleaning is needed because you can only lemmatize and remove stopwords after the translation.
     def __init__(self):
 		pass
+
+    def removeStopwords(self, listOfWords):
+        pass
+
+    def lemmatizeRemoveStopdwords(self, dataframe):
+        pass
 ######################################################################
 class Tagging_Subsetting:
     def __init__(self):
 		pass
+
+    def removeStopwords(self, listOfWords):
+        pass
+
+    def tagging(self, dataframe):
+        pass
+
+    def filter(self, dataframe):
+        pass
 ######################################################################
 class Balancing:
     def __init__(self):
 		pass
+
+    def balance_Data(self, x, y, bt):
+        pass
 ######################################################################
 class Feature_Selection:
     def __init__(self):
 		pass
+
+    def feature_Selection(self, db, bt):
+        pass
 ######################################################################
 class Model_Creation:
     def __init__(self):
 		pass
+
+    def run_Model(self, modelName, modelParameters):
+        """
+        Should return models with and without feature selection and every balancing technique
+        "parameters" should be something args (?) that will be given to the model function
+        """
+        pass
+
+    def run_Evaluation(self, modelName):
+        """
+        Returns all given evaulation score/plots for every version the given model
+        """
+        pass
