76f610815a818716d5e395ba8d2d9b65e9174168
Max Kinner <maximiliankinner@gmail.com>
2020-10-19 17:14:14 +0200

    Conversion of pd.series to string

--- scripts/datapreparation/extraction/post_migration_hash_taglist_onlytext.py
+++ scripts/datapreparation/extraction/post_migration_hash_taglist_onlytext.py
@@ -4,16 +4,19 @@ from pymongo import MongoClient
 
 
 client = MongoClient('localhost', 27017)
-db = client['migrate_instagram'] 
-collection = db.instagram_post
-data_post = pd.DataFrame(list(collection.find({})))
-df_post = data_post.copy()
+db = client['02_NationalIdentity_Cleaned'] 
+collection = db.Instagram_Post_Cleaned
+df_post = pd.DataFrame(list(collection.find({})))
+
+##TODO Use "03_Combined" data
 
 def decorator_taglist(func):
     def wrapper_function(value):
         #taglist=value['post_Caption'].str.findall(r'@.*?(?=\s|$)')
         hashtag_removed = func(value)
         hashtag_removed['tagList'] = hashtag_removed['post_Caption'].str.findall(r'@.*?(?=\s|$)')
+        hashtag_removed['tagList'] = pd.Series(hashtag_removed['tagList'], dtype="string")
+        hashtag_removed['tagList'] =  hashtag_removed['tagList'].str.strip("[]")
         hashtag_removed['onlyText'] = hashtag_removed['onlyText'].str.replace(r'@.*?(?=\s|$)',"")
         return hashtag_removed
     return wrapper_function
@@ -21,6 +24,7 @@ def decorator_taglist(func):
 def decorator_hashtag(func): 
     def wrapper_function(value): 
         value['hashtag'] = value['post_Caption'].str.findall(r'#.*?(?=\s|$)')
+        value['hashtag'] = pd.Series(value['post_Caption'], dtype="string")
         value['onlyText'] = value['post_Caption'].str.replace(r'#.*?(?=\s|$)',"")
         return value
     return wrapper_function
@@ -33,4 +37,9 @@ def hastag_taglist_onlytext_function(post):
 
 result1=hastag_taglist_onlytext_function(df_post)
 print(result1)
-result1.to_csv('test4.csv')
+result1['hashtag'] = result1['hashtag'].to_string()
+#print(type(result1["hashtag"]))
+
+print(type(result1["tagList"][0]))
+#result1.to_csv('test4.csv')
+print(result1['tagList'])
