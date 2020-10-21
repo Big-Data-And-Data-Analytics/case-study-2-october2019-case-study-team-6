71c5d81f0bbd129c4900bc955a83e95c7e8f9625
varad dingankar <vmdhhh@gmail.com>
2020-10-21 18:19:39 +0200

    mongoConnection function change

--- scripts/datapreparation/mongoConnection.py
+++ scripts/datapreparation/mongoConnection.py
@@ -1,14 +1,10 @@
 from pymongo import MongoClient
 import pandas as pd
 
-def connectMongo(db, col):
+def getCollection(db, col):
     conn = MongoClient("localhost" , 27017)
     collobj = conn[db][col]
     collection = pd.DataFrame(list(collobj.find({})))
-    return collection.copy()
-
-def getCollection(col):
-    collection = pd.DataFrame(list(col.find({})))
     return collection.copy()
 
 def insertCollection(db, col, result):
