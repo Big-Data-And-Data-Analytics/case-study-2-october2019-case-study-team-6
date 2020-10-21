6dc74233c6f9d39c01e3429c8a8518b11084c7b6
varad dingankar <vmdhhh@gmail.com>
2020-10-21 17:24:44 +0200

    Adding class in extraction of hashtag and tag

--- scripts/datapreparation/mongoConnection.py
+++ scripts/datapreparation/mongoConnection.py
@@ -8,3 +8,9 @@ def connectMongo(db, col):
 def getCollection(col):
     collection = pd.DataFrame(list(col.find({})))
     return collection.copy()
+
+def insertCollection(db, col, result):
+    result = result.to_dict("records")
+    conn = MongoClient("localhost" , 27017)
+    connObj =  conn[db][col]
+    connObj.insert_many(result)
