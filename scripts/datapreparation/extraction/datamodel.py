a25a4aa9a8a38ee160fcb021e909d6bdd464dfed
Max Kinner <maximiliankinner@gmail.com>
2020-11-01 19:18:58 +0100

    Added mongoConnection docstring; Added datamodel advancements

--- scripts/datapreparation/extraction/datamodel.py
+++ scripts/datapreparation/extraction/datamodel.py
@@ -1,72 +1,101 @@
+from pymongo import MongoClient
+import pandas as pd
+#import scripts.mongoConnection as mongocon
+
 class DataModel:
-    """DataModel class provides
+    """DataModel class provides functions to apply the datamodel on the given datasource and combines them into a unified shape
     """
 
     def __init__(self):
         pass
 
-    def apply_Post(self):
-        pass
+    def rename_columns_post(self, data, source_col_names):
+        """[summary]
 
-    def apply_Comments(self):
-        pass
+        :param data: [description]
+        :type data: [type]
+        :param source_col_names: [description]
+        :type source_col_names: [type]
+        :return: [description]
+        :rtype: [type]
+        """
+        data_renamed = data.rename(columns={
+            source_col_names[0] : "Id",
+            source_col_names[1] : "username",
+            source_col_names[2] : "text",
+            source_col_names[3] : "likes",
+            source_col_names[4] : "timestamp",
+            source_col_names[5] : "owner_id",
+            source_col_names[6] : "source_type",
+            source_col_names[7] : "data_type"
+        })
+        return data_renamed
 
-    def apply_SubComments(self):
-        pass
+if __name__ == "__main__":
 
-    def rename_Columns(self, **kwargs, data_Type, source_Type):
-        """Takes a given set of source column names to rename them into the specified data model
-        """        
-        pass
+################## REMOVE AGAIN BECAUSE PROBLEM WITH IMPORT
+    def getCollection(db, col):
+        """Retruns the data for a given database and collection
 
-######################################
-from pymongo import MongoClient
-import pandas as pd
-
-# Source
-client = MongoClient('localhost', 27017)
-db = client['02_NationalIdentity_Cleaned']
-youTube_Video_Stats_Cleaned = db.youTube_Video_Stats_Cleaned	
-youTube_Video_Stats_Cleaned = pd.DataFrame(list(youTube_Video_Stats_Cleaned.find({})))
-youTube_Video_Stats_Cleaned = youTube_Video_Stats_Cleaned.drop(columns="_id")
-
-youTube_Videos_Cleaned = db.youTube_Videos_Cleaned	
-youTube_Videos_Cleaned = pd.DataFrame(list(youTube_Videos_Cleaned.find({})))
-youTube_Videos_Cleaned = youTube_Videos_Cleaned.drop(columns="_id")
-
-youTube_Videos_Data = youTube_Videos_Cleaned.set_index("video_id").join(youTube_Video_Stats_Cleaned.set_index("video_id"), on="video_id", how="left", lsuffix="_caller")
-
-youTube_Videos_Data["source_type"] = "YouTube"
-youTube_Videos_Data["data_type"] = "post"
-
-youTube_Videos_Data = youTube_Videos_Data.reset_index()
-
-youTube_Videos_Data = youTube_Videos_Data[[
-    "video_id",
-    "channel",
-    "description",
-    "viewCount",
-    "publishedAt",
-    "channel",
-    "source_type",
-    "data_type"]]
-
-youTube_Common_Data_Post = youTube_Videos_Data.rename(columns={
-    "video_id" : "Id",
-    "channel" : "username",
-    "description" : "text",
-    "viewCount" : "likes",
-    "publishedAt" : "timestamp",
-    "channel" : "owner_id",
-    "source_type" : "source_type",
-    "data_type" : "data_type"
-    })
+        :param db: Database that should be returned
+        :type db: String
+        :param col: Collection that should be returned
+        :type col: String
+        :return: Data from the given collection
+        :rtype: pandas dataframe
+        """    
+        conn = MongoClient("localhost" , 27017)
+        collobj = conn[db][col]
+        collection = pd.DataFrame(list(collobj.find({})))
+        return collection.copy()
+##################
 
 
-insert_data = youTube_Common_Data_Post.to_dict("records")
+#### YOUTUBE POST ####
+    # Get the data from MongoDB
+    youTube_Video_Stats_Cleaned = getCollection("02_NationalIdentity_Cleaned", "youTube_Video_Stats_Cleaned")
+    youTube_Videos_Cleaned = getCollection("02_NationalIdentity_Cleaned", "youTube_Videos_Cleaned")
 
-# Target
-db = client['02_NationalIdentity_Combined_Test']
-collection = db.youTubeJoinTest
-db.drop_collection(collection)
-result = collection.insert_many(insert_data)
+    youTube_Video_Stats_Cleaned = youTube_Video_Stats_Cleaned.drop(columns="_id")
+    youTube_Videos_Cleaned = youTube_Videos_Cleaned.drop(columns="_id")
+
+    youTube_Videos_Data = youTube_Videos_Cleaned.set_index("video_id").join(youTube_Video_Stats_Cleaned.set_index("video_id"), on="video_id", how="left", lsuffix="_caller")
+
+    youTube_Videos_Data["source_type"] = "YouTube"
+    youTube_Videos_Data["data_type"] = "post"
+
+    # Instanceiate class
+    apply_model = DataModel()
+
+    youTube_Videos_Data = apply_model.rename_columns_post(youTube_Videos_Data, source_col_names=[
+        "video_id",
+        "channel",
+        "description",
+        "viewCount",
+        "publishedAt",
+        "channel",
+        "source_type",
+        "data_type"])
+
+    youTube_Videos_Data = youTube_Videos_Data.reset_index()
+
+#### YOUTUBE COMMENT ####
+    youTube_Video_Comments_Cleaned = getCollection("02_NationalIdentity_Cleaned", "youTube_Video_Comments_Cleaned")
+
+    youTube_Video_Comments_Cleaned = youTube_Video_Comments_Cleaned.drop(columns="_id")
+
+    youTube_Video_Comment = youTube_Video_Comments_Cleaned["parentId"].dropna()
+    youTube_Video_Comment = youTube_Video_Comments_Cleaned.join(youTube_Video_Comment, on="parentId", how="right", lsuffix="_caller")
+    
+    youTube_Videos_Data = apply_model.rename_columns_post(youTube_Videos_Data, source_col_names=[
+        "Id",
+        "Comment_Id",
+        "Comment_Owner",
+        "Comment",
+        "Comment_Likes",
+        "Comment_Time_Posted",
+        "hashtags",
+        "TagList",
+        "onlyText",
+        "source_Type",
+        "data_Type"])
