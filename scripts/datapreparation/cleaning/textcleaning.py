e0bb8b4c0f676710e05f4d6fca25f062ccc2705a
MaxKinner <58422098+MaxKinner@users.noreply.github.com>
2020-11-20 14:42:33 +0100

    renamed textcleaning file; minor changes to textcleaning

--- scripts/datapreparation/cleaning/textcleaning.py
+++ scripts/datapreparation/cleaning/textcleaning.py
@@ -6,7 +6,6 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from scripts.mongoConnection import getCollection, insertCollection
 
-
 class TextCleaning:
     """TextCleaning class represents a class for cleaning a dataframe column onlyText
     where stop words will be removed and text will be lemmatized
@@ -33,24 +32,24 @@ class TextCleaning:
         lemmatized_output = ' '.join([self.lemmatizer.lemmatize(w) for w in filtered_list])
         return lemmatized_output
 
-    def get_clean_df(self, df):
+    def get_clean_df(self, df, col):
         """cleans onlyText column of the given dataframe and inserts that dataframe in the '07_PreProcessing' database
         
         :param1 df: Dataframe to Clean
         :type1 df: Pandas Dataframe
         """
 
-        df.dropna(subset=['onlyText'], inplace=True)
-        df['onlyText'] = df['onlyText'].str.lower()
-        df['onlyText'].replace(r'@', '', regex=True, inplace=True)
-        df['onlyText'].replace(r'http\S+', '', regex=True, inplace=True)
-        df['onlyText'].replace(r'http \S+', '', regex=True, inplace=True)
-        df['onlyText'].replace(r'www\S+', '', regex=True, inplace=True)
-        df['onlyText'].replace(r'[^\w\s]','', regex=True, inplace=True)
-        df['onlyText'].replace(r'\s\s+',' ', regex=True, inplace=True)
-        df['onlyText'] = df['onlyText'].str.strip()
-        df['onlyText'] = df['onlyText'].apply(word_tokenize)
-        df['onlyText'] = df['onlyText'].apply(self.remove_stop_words)
+        df.dropna(subset=[col], inplace=True)
+        df[col] = df[col].str.lower()
+        df[col].replace(r'@', '', regex=True, inplace=True)
+        df[col].replace(r'http\S+', '', regex=True, inplace=True)
+        df[col].replace(r'http \S+', '', regex=True, inplace=True)
+        df[col].replace(r'www\S+', '', regex=True, inplace=True)
+        df[col].replace(r'[^\w\s]','', regex=True, inplace=True)
+        df[col].replace(r'\s\s+',' ', regex=True, inplace=True)
+        df[col] = df[col].str.strip()
+        df[col] = df[col].apply(word_tokenize)
+        df[col] = df[col].apply(self.remove_stop_words)
         del df['_id']
 
         return df
@@ -60,15 +59,13 @@ if __name__ == '__main__':
     textCleaning = TextCleaning()
 
     df_post = getCollection('06_NationalIdentity_Translated', 'ni_post_translated')
-    df_post = textCleaning.get_clean_df(df_post)
+    df_post = textCleaning.get_clean_df(df=df_post, col="onlyText")
     insertCollection('07_PreProcessing', 'ni_post_preprocessed', df_post)
 
     df_comment = getCollection('06_NationalIdentity_Translated', 'ni_comment_translated')
-    df_comment = textCleaning.get_clean_df(df_comment)
+    df_comment = textCleaning.get_clean_df(df=df_comment, col="onlyText")
     insertCollection('07_PreProcessing', 'ni_comment_preprocessed', df_comment)
 
     df_subcomment = getCollection('06_NationalIdentity_Translated', 'ni_subcomment_translated')
-    df_subcomment = textCleaning.get_clean_df(df_subcomment)
+    df_subcomment = textCleaning.get_clean_df(df=df_subcomment, col="onlyText")
     insertCollection('07_PreProcessing', 'ni_subcomment_preprocessed', df_subcomment)
-
-
