defc4ff36c831af79741b46f2069b076981d63d9
shubham <shubhamshashikantshinde@outlook.com>
2021-01-26 15:47:41 +0100

    New Balancing Function for NI

--- scripts/datapreparation/balancing/balancing.py
+++ scripts/datapreparation/balancing/balancing.py
@@ -14,6 +14,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import Binarizer
 
+
 ##TODO Currently we are using test_train_split function from sklearn, the model shows that 2 classes are not present in the traindata. We should try StratifiedShuffleSplit, as far as i understood it, keep percentage of each class. Implement besides train_test_split
 class BalancingData:
     """BalancingData class represents performance of different balancing data on 'X' and 'y' train data.
@@ -29,16 +30,14 @@ class BalancingData:
         The file path needs to be provided where the output needs to be stored and the entire dataframe collection 
         provided as the input
     """
-    
-    
+
     def __init__(self, filepath, new_data):
         self.filepath = filepath
         self.new_data = new_data
 
-    #TODO: Generalize the function if possible
+    # TODO: Generalize the function if possible
     def split_train_test(self, test_size, random_state):
-
-        """ 'X' variable is assigned 'onlyText' column and 'y' variable has the 'identityMotive'.  The 'X' and 'y' are 
+        """ 'X' variable is assigned 'onlyText' column and 'y' variable has the 'identityMotive'.  The 'X' and 'y' are
         then divided into test and train data.
         The input for different balancing techniques must be in vectorised form. Thus, count vectoriser is applied on
         'X'. 
@@ -60,7 +59,7 @@ class BalancingData:
 
         vocab = dict()
         vocab['dict'] = str(cv.vocabulary_)
-        vocab = pd.DataFrame(vocab, index=['vocab',])
+        vocab = pd.DataFrame(vocab, index=['vocab', ])
         insertCollection('09_TrainingData', 'CountVectorVocabulary', vocab)
 
         vocab = cv.vocabulary_
@@ -72,10 +71,44 @@ class BalancingData:
         y_test.insert(0, 'id', range(0, len(y_test)))
         insertCollection('09_TrainingData', 'y_test', y_test)
 
+    def split_train_test_ni(self, test_size, random_state):
+        """ 'X' variable is assigned 'onlyText' column and 'y' variable has the 'identityMotive'.  The 'X' and 'y' are
+        then divided into test and train data.
+        The input for different balancing techniques must be in vectorised form. Thus, count vectoriser is applied on
+        'X'.
+        The vocabulary is saved into CountVectorVocabulary. The results of 'X_test' is saved in the provided filepath
+        and 'y_test' is inserted into the database.
+
+        :param new_data: complete collection
+        :type new_data: dataframe
+        """
+        X = self.new_data[['onlyText']]
+        y = self.new_data[['identityMotive']]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+
+        print(type(X_train))
+        cv = CountVectorizer()
+        cv.fit(X_train['onlyText'])
+        self.X_train = cv.transform(X_train['onlyText'])
+        self.y_train = y_train
+
+        vocab = dict()
+        vocab['dict'] = str(cv.vocabulary_)
+        vocab = pd.DataFrame(vocab, index=['vocab', ])
+        insertCollection('09_TrainingData', 'CountVectorVocabulary', vocab)
+
+        vocab = cv.vocabulary_
+        cv_test = CountVectorizer(vocabulary=vocab)
+        cv_test.fit(X_test['onlyText'])
+        X_test = cv_test.transform(X_test['onlyText'])
+
+        scipy.sparse.save_npz(self.filepath + 'X_test.npz', X_test)
+        y_test.insert(0, 'id', range(0, len(y_test)))
+        insertCollection('09_TrainingData_Ni', 'y_test', y_test)
+
     #### FIT BALANCING #### Multi Threading Part
     # Thread1 # ADASYN
     def thread1_ADASYN(self, x, y):
-
         """An object of the class ADASYN is created and the dataset is resampled using 'fit_resample'. The output
         'X_res'  of the resampling is saved in the provided path and the output of 'y_res' is stored into the database.
 
@@ -95,13 +128,11 @@ class BalancingData:
         ## Save y_res
         y_res.insert(0, 'id', range(0, len(y_res)))
         insertCollection('09_TrainingData', 'ADASYN_y', y_res)
-        
-        print("ADASYN saved and done")
 
+        print("ADASYN saved and done")
 
     # Thread 2 # SMOTE
     def thread2_SMOTE(self, x, y):
-
         """An object of the class SMOTE is created and the dataset is resampled using 'fit_resample'. The output
         'X_sm'  of the resampling is saved in the provided path and the output of 'y_sm' is stored into the database.
 
@@ -124,10 +155,8 @@ class BalancingData:
 
         print("SMOTE saved and done")
 
-
     # Thread 3 # SMOTEENN
     def thread3_SMOTEENN(self, x, y):
-
         """An object of the class SMOTEENN is created and the dataset is resampled using 'fit_resample'. The output
         'X_se'  of the resampling is saved in the provided path and the output of 'y_se' is stored into the database.
 
@@ -150,10 +179,8 @@ class BalancingData:
 
         print("SMOTEENN saved and done")
 
-
     # Thread 4 # SMOTETomek
     def thread4_SMOTETomek(self, x, y):
-
         """An object of the class SMOTETomek is created and the dataset is resampled using 'fit_resample'. The output
         'X_st'  of the resampling is saved in the provided path and the output of 'y_st' is stored into the database.
 
@@ -177,10 +204,8 @@ class BalancingData:
 
         print("SMOTETomek saved and done")
 
-
     # Thread 5 # NearMiss
     def thread5_NearMiss(self, x, y):
-
         """An object of the class NearMiss is created and the dataset is resampled using 'fit_resample'. The output
         'X_nm'  of the resampling is saved in the provided path and the output of 'y_nm' is stored into the database.
 
@@ -204,11 +229,8 @@ class BalancingData:
 
         print("NearMiss saved and done")
 
-
     # Thread 6 # TomekLinks
     def thread6_TomekLinks(self, x, y):
-
-        
         """An object of the class TomekLinks is created and the dataset is resampled using 'fit_resample'. The output
         'X_tl'  of the resampling is saved in the provided path and the output of 'y_tl' is stored into the database.
 
@@ -233,25 +255,26 @@ class BalancingData:
         print("TomekLinks saved and done")
 
     def threading_function(self):
-
-        """Different threads are created for each balancing technique having the target as the functions of 
+        """Different threads are created for each balancing technique having the target as the functions of
         balancing techniques and arguments are the inputs i.e 'X_train' and 'y_train'. After the creation of threads,
         the threads are executed.
         """
-        t1 = threading.Thread(name = "thread1_ADASYN",      target = self.thread1_ADASYN,       args = (self.X_train, self.y_train))
-        t2 = threading.Thread(name = "thread2_SMOTE",       target = self.thread2_SMOTE,        args = (self.X_train, self.y_train))
-        t3 = threading.Thread(name = "thread3_SMOTEENN",    target = self.thread3_SMOTEENN,     args = (self.X_train, self.y_train))
-        t4 = threading.Thread(name = "thread4_SMOTETomek",  target = self.thread4_SMOTETomek,   args = (self.X_train, self.y_train))
-        t5 = threading.Thread(name = "thread5_NearMiss",    target = self.thread5_NearMiss,     args = (self.X_train, self.y_train))
-        t6 = threading.Thread(name = "thread6_TomekLinks",  target = self.thread6_TomekLinks,   args = (self.X_train, self.y_train))
-   
+        t1 = threading.Thread(name="thread1_ADASYN", target=self.thread1_ADASYN, args=(self.X_train, self.y_train))
+        t2 = threading.Thread(name="thread2_SMOTE", target=self.thread2_SMOTE, args=(self.X_train, self.y_train))
+        t3 = threading.Thread(name="thread3_SMOTEENN", target=self.thread3_SMOTEENN, args=(self.X_train, self.y_train))
+        t4 = threading.Thread(name="thread4_SMOTETomek", target=self.thread4_SMOTETomek,
+                              args=(self.X_train, self.y_train))
+        t5 = threading.Thread(name="thread5_NearMiss", target=self.thread5_NearMiss, args=(self.X_train, self.y_train))
+        t6 = threading.Thread(name="thread6_TomekLinks", target=self.thread6_TomekLinks,
+                              args=(self.X_train, self.y_train))
+
         t1.start()
         t2.start()
         t3.start()
         t4.start()
         t5.start()
-        t6.start() 
-        
+        t6.start()
+
         t1.join()
         t2.join()
         t3.join()
@@ -267,13 +290,13 @@ if __name__ == "__main__":
     df_source_collection = getCollection('08_PreTrain', 'train_data')
 
     filepath = "D:/OneDrive - SRH IT/06 Case Study I/02 Input_Data/03 Model/NPZs/"
-    #filepath = input("Enter the path of your file with '/': ")
-    
+    # filepath = input("Enter the path of your file with '/': ")
+
     if os.path.isdir(filepath):
         f = open(r"filepath", "w")
     else:
-        print ("Directory does not exist.")
+        print("Directory does not exist.")
 
-    balancing_input = BalancingData(filepath,df_source_collection)
+    balancing_input = BalancingData(filepath, df_source_collection)
     balancing_input.split_train_test(test_size=0.25, random_state=69)
     balancing_input.threading_function()
