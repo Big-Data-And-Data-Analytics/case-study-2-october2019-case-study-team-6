8177f6deb6c633d8c5652bd0cdc74c7364439df1
shubham <shubhamshashikantshinde@outlook.com>
2021-03-03 13:34:07 +0100

    Renaming Files

--- scripts/chrome_plugin/predict_id_motive.js
+++ scripts/chrome_plugin/predict_id_motive.js
@@ -5,22 +5,14 @@
     alert(selected_Text);
     
     const req = new XMLHttpRequest();
-//    const baseUrl = "http://localhost:5000/models_IM";
+
     const baseUrl = "http://localhost:5000/predict_id_motive";
     var body = XMLHttpRequest.response;
 
-//    var param = "sentence=" + selected_Text + "&modelNumber='0'"
     var data = {sentence: selected_Text, modelNumber: "0"};
-//    var data = "{'sentence': 'selected_Text', 'modelNumber': '0'}";
-//    http://localhost:5000/predict_id_motive?sentence="selected_Text"&modelNumber="0"
-
     req.open(method="POST", url=baseUrl, true);
-//    req.open(method="GET", url=baseUrl, true);
-//    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
     req.setRequestHeader("Content-Type", "application/json");
-//    alert(JSON.stringify(data))
     req.send(JSON.stringify(data));
-//    req.send(data);
     req.onreadystatechange = function() { // Call a function when the state changes.
         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
             alert("Got response 200!");
