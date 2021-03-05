4ef3068a34d55379c4db7c1d0ea3052dc4925e2e
Max Kinner <maximiliankinner@gmail.com>
2021-03-05 19:24:42 +0100

    Added test display of predicted output in popup

--- scripts/chrome_plugin/button_logic.js
+++ scripts/chrome_plugin/button_logic.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var id_button = document.getElementById('id_button');
     var ni_button = document.getElementById('ni_button');
+    var test_button = document.getElementById('test_button');
 
     id_button.onclick = predict_id_motive;
     function predict_id_motive() {
-        alert("Predicting id...")
         {
             chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
                 chrome.tabs.executeScript(tabs[0].id, {
@@ -16,7 +16,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     ni_button.onclick = predict_ni;
     function predict_ni() {
-        alert("Predicting ni...")
         {
             chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
                 chrome.tabs.executeScript(tabs[0].id, {
@@ -25,4 +24,22 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         };
     }
+
+    test_button.onclick = test;
+    function test() {
+        {
+            chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+                chrome.tabs.executeScript(tabs[0].id, {
+                    file: 'output_display_test.js'
+                });
+
+                chrome.runtime.onMessage.addListener(
+                    function(request, sender, sendResponse) {
+                        document.getElementById("extensionpopupcontent").innerHTML = request.payload;
+                    }
+                );
+            });
+        };
+    }
+
 }, false);
